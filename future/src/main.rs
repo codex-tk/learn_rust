e94b2cbb40f3ed5d40cac8d4559a99e607b23935
tlab <gwonhyeong.tak@gmail.com>
2019-10-22 00:52:29 +0900

    [title] future executor sample

--- future/src/main.rs
+++ future/src/main.rs
@@ -8,10 +8,51 @@ struct PrintPolledFuture;
 impl Future for PrintPolledFuture {
     type Output = ();
 
-    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output>
-    {
+    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
         println!("Polled!");
         Poll::Ready(())            
+    }
+}
+
+struct Executor{
+    waker : Waker,
+}
+
+impl Executor {
+    fn new() -> Executor {
+
+        fn raw_waker_clone(_a:*const())->std::task::RawWaker{
+            panic!("unimplemented");
+        }
+
+        fn raw_waker_all(_a:*const()){
+            panic!("unimplemented");
+        }
+
+        fn raw_waker_drop(_a: *const ()){}
+
+        static RAW_WAKER_VTABLE : RawWakerVTable = RawWakerVTable::new(
+                                                        raw_waker_clone,
+                                                        raw_waker_all ,
+                                                        raw_waker_all,
+                                                        raw_waker_drop);
+        Executor{
+            waker : unsafe {
+                Waker::from_raw(
+                    RawWaker::new(ptr::null(),&RAW_WAKER_VTABLE))
+            }
+        }
+    }
+
+    fn block_on<T>(&mut self, mut f : T ) -> T::Output
+        where T : Future {
+        loop{
+            let mut context = Context::from_waker(&self.waker);
+            match unsafe { Pin::new_unchecked(&mut f).poll(&mut context)} {
+                Poll::Ready(val) => return val,
+                Poll::Pending => continue,
+            }
+        }
     }
 }
 
@@ -25,29 +66,7 @@ async fn run_print_polled_future(){
     v.await;
 }
 
-fn raw_waker_clone(_a:*const())->std::task::RawWaker{
-    panic!("unimplemented");
-}
-
-fn raw_waker_all(_a:*const()){
-    panic!("unimplemented");
-}
-
-fn raw_waker_drop(_a: *const ()){}
 
 fn main() {
-    static RAW_WAKER_VTABLE : RawWakerVTable = RawWakerVTable::new(
-                                                        raw_waker_clone,
-                                                        raw_waker_all ,
-                                                        raw_waker_all,
-                                                        raw_waker_drop);
-    let waker = unsafe {
-        Waker::from_raw(
-            RawWaker::new(ptr::null(),&RAW_WAKER_VTABLE))
-    };
-    let mut context = Context::from_waker(&waker);
-    let mut fut = run_print_polled_future();
-    let _ = unsafe {
-         Pin::new_unchecked(&mut fut).poll(&mut context)
-    };
+    Executor::new().block_on(run_print_polled_future());
 }
